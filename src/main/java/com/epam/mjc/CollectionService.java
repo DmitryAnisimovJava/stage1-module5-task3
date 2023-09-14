9022d5e263c2ebcead44a4af1e4949ca8e365b33
DmitryAnisimovJava <130083398+DmitryAnisimovJava@users.noreply.github.com>
2023-09-14 14:56:34 +0300

    Complete all tasks.

--- src/main/java/com/epam/mjc/CollectionService.java
+++ src/main/java/com/epam/mjc/CollectionService.java
@@ -1,27 +1,44 @@
 package com.epam.mjc;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class CollectionService {
 
     public List<Integer> filterEvenNumbers(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        List<Integer> resultList = list.stream()
+        		.filter(numberFromList -> numberFromList % 2 == 0)
+        		.collect(Collectors.toList());
+        return resultList;
     }
 
     public List<String> toUpperCaseCollection(List<String> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        List<String> resultList = list.stream()
+        		.map(String::toUpperCase)
+        		.collect(Collectors.toList());
+        return resultList;
     }
 
     public Optional<Integer> findMax(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        Optional<Integer> resultOptional = list
+        		.stream()
+        		.max(Comparator.naturalOrder());
+        return resultOptional;
     }
 
     public Optional<Integer> findMin(List<List<Integer>> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        Optional<Integer> findMinOptional = list
+        		.stream()
+        		.flatMap(listElement -> listElement.stream())
+        		.min(Comparator.naturalOrder());
+        return findMinOptional;
     }
 
     public Integer sum(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        int sum = list.stream().mapToInt(x -> x).sum();
+        return sum;
     }
 }
